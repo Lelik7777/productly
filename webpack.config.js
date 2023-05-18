c3eebcee332adc6dc65b52ad5f4a99ac56d28a84
Lelik Jan <21alex7777@gmail.com>
2023-05-18 10:17:52 +0300

    feat: install webpack again

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,13 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const isProduction = process.env.NODE_ENV == 'production';
 
 
-const stylesHandler = MiniCssExtractPlugin.loader;
+const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
 
 
 
 const config = {
     entry: './src/index.js',
+    devtool:'source-map',
     output: {
         path: path.resolve(__dirname, 'dist'),
     },
@@ -25,13 +26,16 @@ const config = {
             template: 'index.html',
         }),
 
-        new MiniCssExtractPlugin(),
-
         // Add your plugins here
         // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
     module: {
         rules: [
+            {
+                test: /\.(ts|tsx)$/i,
+                loader: 'ts-loader',
+                exclude: ['/node_modules/'],
+            },
             {
                 test: /\.css$/i,
                 use: [stylesHandler,'css-loader'],
@@ -44,18 +48,27 @@ const config = {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
             },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+              },
 
             // Add your rules for custom modules here
             // Learn more about loaders from https://webpack.js.org/loaders/
         ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
     },
 };
 
 module.exports = () => {
     if (isProduction) {
         config.mode = 'production';
-        
-        
+
+        config.plugins.push(new MiniCssExtractPlugin());
+
+
     } else {
         config.mode = 'development';
     }
