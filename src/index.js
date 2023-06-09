d3320dc414c0ce4b1bfced7ec66022cbe97b19b5
Lelik Jan <21alex7777@gmail.com>
2023-06-09 19:24:57 +0300

    feat add Modal.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import { addTagClickHandler } from "./js/strategies";
+import { Modal } from "./js/Modal";
 
 window.addEventListener("load", function () {
   addTagClickHandler();
+  renderStrategyModal("content for modal");
 });
+
+const renderStrategyModal = (content) => {
+  const modalStrategy = new Modal('modal','modal__strategy');
+  modalStrategy.buildModal(content);
+  console.log('render modal');
+};
