aa540477afb66d508b372910c57f9cd67f107454
Lelik Jan <21alex7777@gmail.com>
2023-06-11 18:22:03 +0300

    fix:fix bags in strategy.js

--- src/js/data.js
+++ src/js/data.js
@@ -1,7 +1,7 @@
 import img1 from "../assets/images/strategies/strategy_1.jpg";
 import img2 from "../assets/images/strategies/strategy_2.jpg";
 import img3 from "../assets/images/strategies/strategy_3.jpg";
-import img4 from "../assets/images/strategies/strategy_4_tablet.jpg";
+import img4 from "../assets/images/strategies/strategy_4_376.jpg";
 import img5 from "../assets/images/strategies/strategy_5.jpg";
 
 export const DATA = [
@@ -75,6 +75,15 @@ export const DATA = [
     title: "Success Steps For Your Personal Or Business Life",
     urlToImage: img3,
     tags: ["Culture", "Design", "Art"],
+    content:
+      "Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?",
+    date: "01.01.2020",
+  },
+  {
+    id: 9,
+    title: "Increasing Prosperity With Positive Thinking",
+    urlToImage: img5,
+    tags: ["Design"],
     content:
       "Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?",
     date: "01.01.2020",
--- src/js/strategies.js
+++ src/js/strategies.js
@@ -1,18 +1,14 @@
 import { CONSTANTS, DOM_SELECTORS } from "./config";
 import { UTILS } from "./utils";
 
-//Dom elements
-const _strategiesTags = UTILS.getElementFromDom(DOM_SELECTORS.STRATEGIES_TAGS);
-const _arrayStrategies = UTILS.getElementsFromDom(
-  `${DOM_SELECTORS.STRATEGIES_CONTAINER} ${DOM_SELECTORS.STRATEGY}`
-);
-
 //class names
 const borderedTag = UTILS.deletePointFromSelector(DOM_SELECTORS.TAG_BORDERED);
 const selectedTag = UTILS.deletePointFromSelector(DOM_SELECTORS.TAG_COLORED);
 const hidden = UTILS.deletePointFromSelector(DOM_SELECTORS.STRATEGY_HIDDEN);
 
 export const addTagClickHandler = () => {
+  const _strategiesTags = UTILS.getElementFromDom(DOM_SELECTORS.STRATEGIES_TAGS);
+
   _strategiesTags.addEventListener(CONSTANTS.CLICK, (e) => {
     const tag = e.target;
     if (
@@ -44,7 +40,10 @@ const selectClickedTag = (clickedTag) => {
 };
 
 const showAllStrategies = () => {
-  _arrayStrategies.forEach((strategy) => {
+  const arrayStrategies = UTILS.getElementsFromDom(
+    `${DOM_SELECTORS.STRATEGIES_CONTAINER} ${DOM_SELECTORS.STRATEGY}`
+  );
+  arrayStrategies.forEach((strategy) => {
     strategy.classList.remove(hidden);
   });
 };
